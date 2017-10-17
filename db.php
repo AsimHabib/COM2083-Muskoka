d5307743f7e5bbd32290b5fa893c6f8d03c34a9b
Asim Habib <asimhabib2@gmail.com>
2017-10-17 11:45:21 -0400

    db file updated

--- db.php
+++ db.php
@@ -1,15 +1,21 @@
 <?php
-
+/*
 $db_host = "sql.computerstudi.es";
 $db_name = "gc200305897";
 $db_user = "gc200305897";
 $db_pass = "WYxYzwXX"; 
 
-/*
+
  $db_host = "localhost";
  $db_name = "muskoka_db";
  $db_user = "root";
  $db_pass = ""; */
+ 
+$db_host = "db672705311.db.1and1.com";
+$db_name = "db672705311";
+$db_user = "dbo672705311";
+$db_pass = "WYxYzw@X"; 
+ 
  try{
   
   $conn = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);  
